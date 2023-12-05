a1d48e78c139e70b342b86a7ee54e623763d87e4
neopragma <davenicolette@gmail.com>
2023-12-05 11:41:51 -0700

    Completed FizzBuzz

--- tdd-fizzbuzz/src/main/java/com/neopragma/fizzbuzz/Fizzbuzz.java
+++ tdd-fizzbuzz/src/main/java/com/neopragma/fizzbuzz/Fizzbuzz.java
@@ -3,6 +3,9 @@ package com.neopragma.fizzbuzz;
 public class Fizzbuzz
 {
     public String processNumber(int number) {
-        return number == 3 ? "Fizz" : String.valueOf(number);
+        return number % 15 == 0 ? "FizzBuzz"
+               : number % 5 == 0 ? "Buzz"
+               : number % 3 == 0 ? "Fizz"
+               : String.valueOf(number);
     }
 }
