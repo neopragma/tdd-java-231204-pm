ba0c625e0e3ffd38d3ccebc4e42de4475412b996
neopragma <davenicolette@gmail.com>
2023-12-05 12:44:16 -0700

    Completed RPN calculator

--- tdd-rpn/src/main/java/com/neopragma/rpn/RPN.java
+++ tdd-rpn/src/main/java/com/neopragma/rpn/RPN.java
@@ -1,16 +1,28 @@
 package com.neopragma.rpn;
 
+import java.util.Set;
 import java.util.Stack;
 
-public class RPN
-{
-    private final Stack<String> stack;
-    public RPN() {
-        stack = new Stack<String>();
-    }
-
-    public String enter(String token) {
-        stack.push(token);
+public class RPN {
+    private Stack<String> stack = new Stack<>();
+    private Set<String> operators = Set.of("+", "*", "-", "/");
+    public String enter(String value) {
+        Double value1 = 0.0;
+        Double value2 = 0.0;
+        if (operators.contains(value)) {
+            value1 = Double.valueOf(stack.pop());
+            value2 = Double.valueOf(stack.pop());
+            if (value.equals("+")) {
+                value = String.valueOf(value1 + value2);
+            } else if (value.equals("*")) {
+                value = String.valueOf(value1 * value2);
+            } else if (value.equals("-")) {
+                value = String.valueOf(value1 - value2);
+            } else if (value.equals("/")) {
+                value = String.valueOf(value1 / value2);
+            }
+        }
+        stack.push(value);
         return stack.peek();
     }
 }
--- tdd-rpn/src/test/java/com/neopragma/rpn/RPNTest.java
+++ tdd-rpn/src/test/java/com/neopragma/rpn/RPNTest.java
@@ -2,20 +2,58 @@ package com.neopragma.rpn;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class RPNTest
-{
+public class RPNTest {
+
     private RPN rpn;
 
     @BeforeEach
-    public void beforeEachTestCase() {
+    public void instantiateRPN() {
         rpn = new RPN();
     }
 
     @Test
-    public void entering_a_number_adds_the_number_to_the_stack() {
-        assertEquals("3.75", rpn.enter("3.75"));
+    public void it_returns_the_number_entered() throws Exception {
+        assertEquals("1.0", rpn.enter("1.0"));
     }
+    @Test
+    public void it_returns_the_only_number_entered() throws Exception {
+        rpn.enter("1.0");
+        assertEquals("2.0", rpn.enter("2.0"));
+    }
+
+    @Test
+    public void it_returns_the_last_value_entered() throws Exception {
+        rpn.enter("1.0");
+        rpn.enter("2.0");
+        rpn.enter("+");
+        assertEquals("99.0", rpn.enter("99.0"));
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideValuesForRPNOperations")
+    public void it_handles_addition(
+            String expected, String value1, String value2,
+            String operation, String message) throws Exception {
+        rpn.enter(value1);
+        rpn.enter(value2);
+        assertEquals(expected, rpn.enter(operation), message);
+    }
+
+    private static Stream<Arguments> provideValuesForRPNOperations() {
+        return Stream.of(
+                Arguments.of("9.5", "4.25", "5.25", "+", "Addition"),
+                Arguments.of("15.0", "3.0", "5.0", "*", "Multiplication"),
+                Arguments.of("1.0", "6.0", "7.0", "-", "Subtraction"),
+                Arguments.of("2.0", "3.0", "6.0", "/", "Division"));
+    }
+
+
 }
