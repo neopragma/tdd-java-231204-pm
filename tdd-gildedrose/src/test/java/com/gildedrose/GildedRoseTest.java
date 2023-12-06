dbc837428a3bb903863409dd9d2330bacb9ea7e6
neopragma <davenicolette@gmail.com>
2023-12-06 05:39:45 -0700

    GildedRose partial

--- tdd-gildedrose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ tdd-gildedrose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,11 +7,43 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void conjuredManaCake_decreases_in_value_by_2() {
+        Item[] items = new Item[] { new Item("Conjured Mana Cake", 6, 10) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("foo", app.items[0].name);
+        assertEquals(8, app.items[0].quality);
+    }
+    @Test
+    void backstagePasses_increases_in_value_by_2_when_less_than_10_days_to_go() {
+        Item[] items = new Item[] {
+                new Item("Backstage passes to a TAFKAL80ETC concert", 9, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(12, app.items[0].quality);
+    }
+    @Test
+    void backstagePasses_increases_in_value_by_3_when_less_than_5_days_to_go() {
+        Item[] items = new Item[] {
+                new Item("Backstage passes to a TAFKAL80ETC concert", 4, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(13, app.items[0].quality);
+    }
+    @Test
+    void backstagePasses_increases_in_value_by_1_when_more_than_10_days_to_go() {
+        Item[] items = new Item[] {
+                new Item("Backstage passes to a TAFKAL80ETC concert", 11, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(11, app.items[0].quality);
+    }
+    @Test
+    void Aged_Brie_increases_in_value_by_1_when_sellIn_greater_than_zero() {
+        Item[] items = new Item[] {
+                new Item("Aged Brie", 1, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(11, app.items[0].quality);
     }
 
 }
